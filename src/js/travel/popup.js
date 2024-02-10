fc397d4125bcc559684f4bb9e454dbbccf69a136
Guerzoniansu <alagbehamid16@gmail.com>
2024-02-10 14:23:01 +0000

    Finished index.html

---
+++ src/js/travel/popup.js
@@ -0,0 +1,6 @@
+"use strict";
+var L = require('leaflet');
+var popupTemplate = '<h3>{NAME}</h3>{ACRES} Acres<br><small>Property ID: {PROPERTYID}<small>';
+module.exports = function (feature) {
+    return L.Util.template(popupTemplate, feature.properties);
+};
